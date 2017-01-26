250679659e6af5247eef9d6aff8bb2a6d97075c5
sebas <sebastiangamboa11@gmail.com>
2017-01-26 11:30:30 -0500

    inicio
    maqueta

---
+++ js/frames.js
@@ -0,0 +1,18 @@
+/*$(document).ready(function(){
+    
+    $("#xbox1").click(function(){
+        
+        $("#contenedorArticulos").load("articulos.html");
+        return false;
+        
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}) */
